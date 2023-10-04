7a9edc8a4a405a2600d66a4c5e307ae7c7a12b06
Padmasripriya <padmasripriya17@gmail.com>
2023-10-04 12:24:28 -0500

    added the images folder

--- src/modules/notifications.js
+++ src/modules/notifications.js
@@ -5,21 +5,25 @@ export function Notifications() {
         <div className="notification">
             <div className="notifications-content">
                <p>
+                    <img src="../images/avatar-mark-webber.jpg" />
                     Mark Webber reacted to your recent post My first tournament today! 
                     <br />
                     1m ago
                </p> 
                <p>
+                    <img src="../images/avatar-angela-gray.jpg" />
                     Angela Gray followed you
                     <br />
                     5m ago
                </p>
                <p>
+                    <img src="../images/avatar-jacob-thompson.jpg" />
                     Jacob Thompson has joined your group Chess Club
                     <br />
                     1 day ago
                </p>
                <p>
+                    <img src="../images/avatar-rizky-hasanuddin.jpg" />
                     Rizky Hasanuddin sent you a private message
                     <br />
                     5 days ago
@@ -30,16 +34,19 @@ export function Notifications() {
                     I'm already having lots of fun and improving my game.
                </p>
                <p>
+                    <img src="../images/avatar-kimberly-smith.jpg" />
                     Kimberly Smith commented on your picture
                     <br />
                     1 week ago
                </p>
                <p>
+                    <img src="../images/avatar-nathan-peterson.jpg" />
                     Nathan Peterson reacted to your recent post 5 end-game strategies to increase your win rate
                     <br />
                     2 weeks ago
                </p>
                <p>
+                   <img src="../images/avatar-anna-kim.jpg" />
                     Anna Kim left the group Chess Club
                     <br />
                     2 weeks ago
