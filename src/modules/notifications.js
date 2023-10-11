cc30405035382d49e9577dc8c767c98bdfea3b92
Padmasripriya <padmasripriya17@gmail.com>
2023-10-11 15:26:22 -0500

    added red dots and adjusted some css

--- src/modules/notifications.js
+++ src/modules/notifications.js
@@ -7,7 +7,7 @@ export function Notifications() {
                <p className="flex">
                     <img src={require("../images/avatar-mark-webber.jpg")} className="" alt="image of mark webber"/>
                     <span className="ml-5">
-                         <strong className="text-black">Mark Webber</strong> reacted to your recent post <strong className="hover:text-blue">My first tournament today!</strong>
+                         <strong className="text-black">Mark Webber</strong> reacted to your recent post <strong className="hover:text-blue">My first tournament today!</strong><span className="dot"></span>
                          <br />
                          1m ago
                     </span>
@@ -15,7 +15,7 @@ export function Notifications() {
                <p className="flex">
                     <img src={require("../images/avatar-angela-gray.jpg")} className="w-50px h-50px" alt="image of angela gray"/>
                     <span className="ml-5">
-                         <strong className="text-black">Angela Gray</strong> followed you
+                         <strong className="text-black">Angela Gray</strong> followed you <span className="dot"></span>
                          <br />
                          5m ago
                     </span>
@@ -23,7 +23,7 @@ export function Notifications() {
                <p className="flex">
                     <img src={require("../images/avatar-jacob-thompson.jpg")} className="" alt="image of jacob thompson"/>
                     <span className="ml-5">
-                         <strong className="text-black">Jacob Thompson</strong> has joined your group <strong className="hover:text-blue">Chess Club</strong>
+                         <strong className="text-black">Jacob Thompson</strong> has joined your group <strong className="hover:text-blue">Chess Club</strong> <span className="dot"></span>
                          <br />
                          1 day ago
                     </span>
@@ -37,7 +37,7 @@ export function Notifications() {
                          <br/>
                     </span>
                </p>
-               <p>
+               <p className="comment">
                     Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and 
                     <br />
                     I'm already having lots of fun and improving my game.
@@ -49,6 +49,7 @@ export function Notifications() {
                          <br />
                          1 week ago
                     </span>
+                    <img src={require("../images/image-chess.jpg")} className="chess rounded" alt="image of kimberly smith"/>
                </p>
                <p className="flex">
                     <img src={require("../images/avatar-nathan-peterson.jpg")} className="" alt="image of nathan peterson"/>
